1b2e5f698a340e2b3c972427c7d927b62a3b7de3
Suyash Deshpande <97775667+iSuyash69@users.noreply.github.com>
2024-07-16 16:29:58 +0530

    About me updated

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -13,13 +13,13 @@ const Info = () => {
       <div className="about__box">
         <i class='bx bxs-briefcase about__icon'></i>
         <h3 className="about__title">Projects</h3>
-        <spam className="about__subtitle">100 + Guided</spam>
+        <spam className="about__subtitle">200 + Guided</spam>
       </div>
 
       <div className="about__box">
         <i class='bx bxs-trophy about__icon' ></i>
         <h3 className="about__title">Hackthone</h3>
-        <spam className="about__subtitle">20 + Win</spam>
+        <spam className="about__subtitle">30 + Win</spam>
       </div>
     </div>
   )
